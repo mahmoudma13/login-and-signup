cfbc4a778caaa2349f68c0ce5f3829405a05fd7a
mahmoudma13 <mahmoud13a13@gmail.com>
2024-06-22 21:42:57 +0300

    sec commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ var userName = document.getElementById("user-name");
 if (localStorage.getItem("textHome")) {
     userName.innerHTML = JSON.parse(localStorage.getItem("textHome"));
 } else {
-    window.location.replace("./")
+    window.location.replace("../page/signIn.html");
 }
